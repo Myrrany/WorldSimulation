43707ba47488f07d0727076c7993f1c0cc9bbe71
Myrthe <myrthesakura@gmail.com>
2017-01-14 21:08:45 +0100

    Add comments to GeneticUtils

--- orderchaos/GeneticsUtils.java
+++ orderchaos/GeneticsUtils.java
@@ -5,20 +5,32 @@ import java.util.LinkedHashSet;
 
 public class GeneticsUtils {
 
+	// This set contains the names of all used genes.
+	// It can be extended if needed by adding more names to the list.
 	public static final LinkedHashSet<String> allGeneNames = new LinkedHashSet<String>(
 			Arrays.asList("Hair red", "Hair green", "Hair blue", "Eyes red", "Eyes green", "Eyes blue", "Skin red",
 					"Skin green", "Skin blue", "Wings", "Wings red", "Wings green", "Wings blue", "Tail", "Horns"));
 
+	// Constructor
 	public GeneticsUtils() {
 	}
 
+	// Method to derive a possible genome for a child from the genome of the
+	// parents
+	// Takes the Gene[] of the parents, outputs a Gene[] for the child
 	public static Gene[] breedGenes(Gene[] mom, Gene[] dad) {
 		Gene[] child = new Gene[allGeneNames.size()];
+		// Name some variables for later use.
 		int a;
 		int b;
 		boolean dom;
+		// i is not used to iterate over in the for-loop,
 		int i = 0;
+		// Foreach-loop goes over the genenames in allGeneNames and finds the
+		// corresponding genes in the genome of the parents
 		for (String geneName : allGeneNames) {
+			// Random chance decides which of the two genes a parent will pass
+			// on.
 			if (Math.random() > 0.5) {
 				a = mom[i].getChromA();
 			} else {
@@ -29,11 +41,14 @@ public class GeneticsUtils {
 			} else {
 				b = dad[i].getChromB();
 			}
+			// Check if a gene is dominant and pass it on.
 			if (mom[i].isDom() == dad[i].isDom()) {
 				dom = mom[i].isDom();
 			} else {
 				dom = true;
 			}
+			// Store all previously gathered information in a gene and put it in
+			// the childs genome.
 			child[i] = new Gene(a, b, dom);
 			child[i].setGeneName(geneName);
 			i++;
@@ -41,19 +56,29 @@ public class GeneticsUtils {
 		return child;
 	}
 
+	// Method to print a Gene[] to string
+	// Basically a toString method
 	public static String printGenes(Gene[] array) {
 		StringBuilder sb = new StringBuilder();
+		// Iterates over the given array
 		for (int i = 0; i < array.length; i++) {
+			// Uses the Gene.toString method and appends it to a string builder.
 			sb.append(array[i].toString());
+			// Adds newlines for readability
 			sb.append("\n\n");
 		}
+		// Gives the end result
 		return sb.toString();
 	}
 
+	// Method to randomly generate a Gene[]
 	public static Gene[] generateGenome() {
 		Gene[] name = new Gene[allGeneNames.size()];
 		int i = 0;
+		// Iterates over all genenames and generates a gene
 		for (String geneName : allGeneNames) {
+			// Uses other parameters for gene generation if the gene is for the
+			// presence of certain body parts
 			if (geneName.equals("Wings") || geneName.equals("Tail") || geneName.equals("Horns")) {
 				name[i] = new Gene(false, 0, 1);
 			} else {
@@ -66,3 +91,8 @@ public class GeneticsUtils {
 	}
 
 }
+
+
+
+
+// Ceterum censeo Carthaginem esse delendam
