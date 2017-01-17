046933c7970be4075f66bcbc49350591dde7f2db
Myrthe <myrthesakura@gmail.com>
2017-01-17 13:28:47 +0100

    Add gene Add possession

--- orderchaos/Demon.java
+++ orderchaos/Demon.java
@@ -2,4 +2,39 @@ package WorldSimulation.orderchaos;
 
 public class Demon extends Being {
 
+	private boolean possessing;
+	private Human possessee;
+
+	public Demon(String name, int age) {
+		super(name, age);
+	}
+
+	public Demon(String name) {
+		super(name);
+	}
+
+	public Demon(int age) {
+		super(age);
+	}
+
+	public Demon() {
+		super();
+	}
+
+	public void posses(Human being) {
+		being.becomePossessed(this);
+		this.possessee = being;
+		this.possessing = true;
+	}
+
+	public void stopPossessing() {
+		this.possessing = false;
+		possessee.deposses();
+		this.possessee = null;
+	}
+
+	public String getPossessionStatus() {
+		return "Possessing " + (this.possessing == true ? this.possessee : "no one.");
+	}
+
 }
--- orderchaos/Human.java
+++ orderchaos/Human.java
@@ -2,13 +2,8 @@ package WorldSimulation.orderchaos;
 
 public class Human extends Being {
 
-	private String name;
-	private int age;
-	private World location;
-	private Being mother;
-	private Being father;
-	private Being partner;
-	
+	private boolean possessed;
+	private Demon possesser;
 	
 	public Human(String name, int age) {
 		super(name, age);
@@ -26,7 +21,18 @@ public class Human extends Being {
 		super();
 	}
 	
-	
+	public void becomePossessed(Demon name) {
+		this.possessed = true;
+		this.possesser = name;
+	}
 
+	public void deposses(){
+		this.possessed = false;
+		this.possesser = null;
+	}
+	
+	public String getPossessionStatus() {
+		return "Possessed by " + (this.possessed == true ? this.possesser : "no one.");
+	}
 	
 }
